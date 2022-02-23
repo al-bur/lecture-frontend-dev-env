4cf02c1f289ee85b773c733a740ba5358de73c0b
sangminsong <ssmin940606@gmail.com>
2022-02-23 22:04:25 +0900

    chore: webpack 설정 추가
    - css-loader
    - style-laoder
    - url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,29 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: {
-    main: './src/app.js'
+    main: './src/app.js',
   },
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
-  }
-}
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|png)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
